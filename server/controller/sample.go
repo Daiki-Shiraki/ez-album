ede3bb63aeb77f3a6c7b3eae7d321dc28bc2a4f7
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-06-15 09:28:06 +0900

    APサーバの環境構築が完了

--- server/controller/sample.go
+++ server/controller/sample.go
@@ -1,5 +1,11 @@
 package controller
 
-func Piyo() bool {
-	return true
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Piyo(c *gin.Context) {
+	c.String(http.StatusOK, "piyo")
 }
--- server/main.go
+++ server/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	_ "ez-album/server/controller"
+	"github.com/Daiki-Shiraki/ez-album/server/controller"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	router := gin.Default()
 
+	router.GET("/piyo", controller.Piyo)
+	// router.GET("/tasks", controller.TaskList)
+	// router.POST("/tasks", controller.CreateTask)
+	// router.GET("/tasks/:id", controller.ReadTask)
+	// router.PUT("/tasks/:id", controller.UpdateTask)
+	// router.DELETE("/tasks/:id", controller.DeleteTask)
+
+	router.Run(":3000")
 }
