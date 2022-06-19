5bde8099d95271f6545d3afd21320ea06c316757
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-06-19 18:07:16 +0900

    tailwind cssの導入 #8

---
+++ client/src/pages/about.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>About Page</title>
+            </Head>
+            <main>
+                <h1>ここは概要ページです。</h1>
+            </main>
+        </div>
+    );
+};
+
+export default About;
