c7fd4a3cf83c02075f7c8c930e2258837bf71033
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-07-01 18:01:20 +0900

    デザインライブラリをChakraUIに変更

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -5,7 +5,6 @@ const MyDocument = () => {
     <Html lang="ja-JP">
       <Head>
         <meta name="application-name" content="MyApp" />
-        <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossOrigin="anonymous"></link>
       </Head>
       <body>
         <Main />
