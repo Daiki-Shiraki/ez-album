b3fbd0f9653fc392840a5526fd2a92cb0e696544
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-07-03 04:07:08 +0900

    react-iconsの導入とアイコン追加

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import Form from "./UploadPhoto";
 import styles from "../styles/MenuBar.module.css";
+import { IconContext } from "react-icons";
+import { HiColorSwatch } from "react-icons/hi";
 
 const MenuBar = () => {
     return (
-        <div className={`pure-menu pure-menu-horizontal ${styles.menubar}`}>
+        <div className={`${styles.menubar}`}>
+            <IconContext.Provider value={{size:'40px'}}>
+                <span className={`${styles.icon}`}><HiColorSwatch/></span>
+            </IconContext.Provider>
             <span className={`${styles.menuText}`}>EZ-Albam</span>
         </div>
     );
