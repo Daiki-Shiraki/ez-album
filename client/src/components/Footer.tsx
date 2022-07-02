2e28e519e1d2eab6ae72bcf967cc63b9ba8bfa11
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-07-03 04:08:00 +0900

    フッターの作成、でも一旦保留

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Footer.module.css"
+
+const Footer = () => {
+    return (
+        <div className={`${styles.footer}`}>
+            <span>msonobe, dshiraki</span>
+        </div>
+    );
+}
+
+export default Footer;
--- client/src/pages/layout.tsx
+++ client/src/pages/layout.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
 import MenuBar from "../components/MenuBar";
 import UploadPhoto from "../components/UploadPhoto";
+import Footer from "../components/Footer";
 
 const Layout = ({ children }: Props) => {
   return (
-    <div>
+    <div style={{width: '100vw', height: '100vh'}}>
         <MenuBar></MenuBar>
         <UploadPhoto></UploadPhoto>
+        {/* <Footer></Footer> */}
         {/* {children} */}
     </div>
   ); 
